9065461bf9694cf04718063c942c24e993137669
davidarogunre <davidarogunre13@outlook.com>
2022-05-10 22:00:39 +0100

    changed default color theme

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -12,6 +12,7 @@ export const ColorModeSwitcher = props => {
       size="md"
       fontSize="lg"
       aria-label={`Switch to ${text} mode`}
+      backgroundColor={text== "light"?"white":null}
       variant="ghost"
       color="current"
       marginLeft="2"
